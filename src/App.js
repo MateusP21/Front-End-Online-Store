32507d88e2349d6c4fe78c7faa77d7943415f88a
MateusP21 <mateuspasso@outlook.com>
2022-03-10 16:10:20 -0300

    requisito 1 - pair programming com todos os integrantes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
+const api = require('./services/api');
 
 function App() {
+  api.getProductsFromCategoryAndQuery('MLB5672', 'Acessórios para Veículos')
+    .then((categories) => { console.log(categories); });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" />
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
+const ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/categories';
+
 export async function getCategories() {
   // Implemente aqui
+  const request = await fetch(ENDPOINT);
+  const json = await request.json();
+  return json;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const ENDPOINT_CATEGORY_AND_QUERY = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const request = await fetch(ENDPOINT_CATEGORY_AND_QUERY);
+  const json = await request.json();
+  return json;
 }
