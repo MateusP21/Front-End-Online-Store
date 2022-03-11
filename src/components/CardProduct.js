5b7a8a553a492199d8e05bc36ac60acff950efa4
MateusP21 <mateuspasso@outlook.com>
2022-03-11 20:35:13 -0300

    again

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import { addProduct } from '../services/productStorage';
 
 class CardProduct extends React.Component {
   render() {
