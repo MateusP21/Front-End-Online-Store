0639db93f990f28f5eacec17cd5ade63e2835d32
MateusP21 <mateuspasso@outlook.com>
2022-03-11 20:22:15 -0300

    fix proptypes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CardProduct from '../components/CardProduct';
 // import { Link } from 'react-router-dom';
 import Carrinho from '../components/Carrinho';
