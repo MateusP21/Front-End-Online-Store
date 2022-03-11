d7561ecda998d40497eb85c51f5356b13008fe70
MateusP21 <mateuspasso@outlook.com>
2022-03-11 16:23:14 -0300

    requisito 9  - pair programming todos presentes

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { readCartProducts, addProduct } from '../services/productStorage';
+import { addProduct } from '../services/productStorage';
 
 class CardProduct extends React.Component {
   render() {
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { getProduct } from '../services/api';
+import { addProduct } from '../services/productStorage';
 
 class ProductDetail extends React.Component {
   constructor() {
@@ -22,11 +23,21 @@ class ProductDetail extends React.Component {
     return (
       <div>
         <Link to="/"> home </Link>
+        <Link data-testid="shopping-cart-button" to="/cart"> carrinho </Link>
         <h3 data-testid="product-detail-name">
           {product.title}
         </h3>
         <span>{product.price}</span>
         <img src={ product.thumbnail } alt="" />
+        <button
+          data-testid="product-detail-add-to-cart"
+          type="button"
+          onClick={ () => addProduct(product) }
+        >
+          {' '}
+          Adicionar ao carrinho
+
+        </button>
       </div>
     );
   }
