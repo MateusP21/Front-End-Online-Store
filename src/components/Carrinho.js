96c5767747deab117bc40a1004cd4b0aeded0a2a
Alexandre <alexandreeduardolemes@gmail.com>
2022-03-14 20:16:41 -0300

    Adiciona folhas de estilo e estiliza ícone do carinho

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../styles/Carrinho.css';
+
+const cartImg = require('../icons/shopping-cart.png');
 
 class Carrinho extends React.Component {
   render() {
     const { cart } = this.props;
     return (
       <Link data-testid="shopping-cart-button" to="/cart">
-        carrinho
-        <p data-testid="shopping-cart-size">
-          { cart.length === 0 ? '0' : cart.length}
-        </p>
+        <div className="cart-group">
+          <img className="cart-img" src={ cartImg } alt="Shopping Cart Icon" />
+          <span className="cart-quantity" data-testid="shopping-cart-size">
+            { cart.length === 0 ? '0' : cart.length}
+          </span>
+        </div>
       </Link>);
   }
 }
